e22ebb42cee8e7d226b433f99a0198d201e333d6
Forest Gregg <fgregg@uchicago.edu>
2015-08-29 16:13:08 -0500

    initial commit

---
+++ fuzzycategory/__init__.py
@@ -0,0 +1,60 @@
+from __future__ import division
+
+import math
+import numpy
+from collections import Counter
+
+class FuzzyCategory(object) :
+    def __init__(self, field_name, corpus, other_fields):
+
+        docs = {}
+        for document in corpus :
+            document = dict(document)
+            field = document.pop(field_name)
+            if field :
+                tokens = set(self._list(document, other_fields))
+                docs.setdefault(field, set()).update(tokens)
+
+        doc_freq = Counter(token 
+                           for tokens in docs.values()
+                           for token in tokens)
+
+        num_docs = len(docs)
+
+        token_weights = {token : math.log(count/num_docs)
+                         for token, count 
+                         in doc_freq.items()
+                         if count > 1}
+
+        self.docs = {}
+        for field, tokens in docs.items() :
+            vector = {token : token_weights[token]
+                      for token in tokens 
+                      if token in token_weights}
+
+            norm = math.sqrt(sum(weight * weight for weight in vector.values()))
+            
+            self.docs[field] = (vector, norm)
+
+    def __call__(self, field_1, field_2):
+        if not field_1 or not field_2 :
+            return numpy.nan
+        
+        elif field_1 == field_2 :
+            return 1.0
+
+        else :
+            vector_1, norm_1 = self.docs[field_1]
+            vector_2, norm_2 = self.docs[field_2]
+
+            if norm_1 and norm_2 :
+                numerator = 0.0
+                for word in set(vector_1) & set(vector_2) :
+                    numerator += vector_1[word] * vector_2[word]
+                return numerator/(norm_1 * norm_2)
+            else :
+                return 0.0
+
+    def _list(self, document, other_fields) :
+        return (document[field] for field in other_fields)
+ 
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+setup(
+    name='fuzzycategory',
+    url='https://github.com/datamade/fuzzycategory',
+    version='0.0.2',
+    description='Simple cosine distance',
+    packages=['fuzzycategory'],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Cython', 
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+    )
+
